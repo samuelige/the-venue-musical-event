0bb7243dda82fe8ab512643b1213293a57fe018d
samuelige <samuelige.iyanu@gmail.com>
2021-05-31 10:55:07 +0100

    Tailwind config

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="text-red-700">
         <h1>Hi, Sammy</h1>
       </header>
     </div>
