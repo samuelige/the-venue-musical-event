1a7759df84a41057d11cbb954559a5d1076d0504
samuelige <samuelige.iyanu@gmail.com>
2021-06-01 15:49:01 +0100

    Added count down function to CountDown component

--- src/components/Banner-Featured/CountDown.js
+++ src/components/Banner-Featured/CountDown.js
@@ -1,24 +1,24 @@
 import React from 'react'
 
-const CountDown = () => {
+const CountDown = ({deadline, days, hours, minutes, seconds}) => {
     return (
-        <div className="border-0 w-72 rounded-lg space-y-3 py-2 px-3 bg-red-700 text-white">
+        <div className="border-0 w-64 md:w-72 rounded-lg space-y-3 py-2 px-2 md:px-3 bg-red-700 text-white">
             <h3>EVENT STARTS IN</h3>
             <div className="flex flex-row text-center items-center ">
-                <div className="border px-4 py-2">
-                    <h1>1</h1>
+                <div className="border px-1 py-2 md:px-4 md:py-2">
+                    <h1>{days}</h1>
                     <p>DAYS</p>
                 </div>
                 <div className="border px-4 py-2">
-                    <h1>1</h1>
+                    <h1>{hours}</h1>
                     <p>HRS</p>
                 </div>
                 <div className="border px-4 py-2">
-                    <h1>4</h1>
+                    <h1>{minutes}</h1>
                     <p>MIN</p>
                 </div>
                 <div className="border px-4 py-2">
-                    <h1>4</h1>
+                    <h1>{seconds}</h1>
                     <p>SEC</p>
                 </div>
             </div>
--- src/sections/banner.js
+++ src/sections/banner.js
@@ -2,27 +2,67 @@ import BFeatured from "../components/Banner-Featured/BFeatured"
 import Carrousel from "../components/Banner-Featured/Carrousel";
 import CountDown from "../components/Banner-Featured/CountDown";
 import Slide from 'react-reveal/Slide';
+import { useState } from "react";
 
 
 const Banner = () => {
+  const [state, setState] = useState({
+    deadline: 'June, 16, 2021',
+    days: '0',
+    hours: '0',
+    minutes: '0',
+    seconds: '0',
+  })
+
+  const {deadline, days, hours, minutes, seconds} = state
+
+
+  const getTimeUnit = (deadline) => {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+
+    if (time < 0) {
+      console.log('Sorry, Event date has passed') 
+    } 
+    else {
+      const seconds = Math.floor((time/1000)%60);
+      const minutes = Math.floor((time/1000/60)%60);
+      const hours = Math.floor((time/(1000*60*60))%24);
+      const days = Math.floor(time/(1000*60*60*24));
+
+      setState({
+        ...state,
+        days: days,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds
+      })
+    }
+  }
+  
+  setInterval(() => {
+    getTimeUnit(deadline)
+  }, );
+
+
+
   return (
     <div className="">
       <div className="absolute w-full">
         <Carrousel/>
       </div>
     
-      <div className="relative w-full top-80">
+      <div className="relative w-full top-56 md:top-60 lg:top-80">
         <BFeatured/>
       </div>
       
-      <div className="w-full relative top-96">
-        <Slide left>
-          <CountDown/>
+      <div className="w-full relative top-72 md:top-72 lg:top-96">
+        <Slide left delay={1000}>
+          <CountDown deadline = {deadline} days={days} hours={hours} minutes={minutes} seconds={seconds}/>
         </Slide>
         
       </div>
     </div>
   );
 }
-{/* <div className="relative w-full"></div> */}
+
 export default Banner
